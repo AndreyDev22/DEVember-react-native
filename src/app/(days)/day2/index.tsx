b66caf0903587170c5d499717a7933940866a6b3
andreyishchenko <andreyishchenko@sprut.ru>
2024-02-05 15:41:29 +0700

    initial day2

---
+++ src/app/(days)/day2/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Day2Page = () => {
+  return (
+    <View>
+      <Text>Day2Page</Text>
+    </View>
+  );
+};
+
+export default Day2Page;
+
+const styles = StyleSheet.create({});
