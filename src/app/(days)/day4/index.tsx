06fb4e863c2d808f2b7f0738adcc873aa24cdfcb
andreyishchenko <andreyishchenko@sprut.ru>
2024-02-07 16:12:52 +0700

    start day4

---
+++ src/app/(days)/day4/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const DetailPageDay4 = () => {
+  return (
+    <View>
+      <Text>DetailPageDay4</Text>
+    </View>
+  );
+};
+
+export default DetailPageDay4;
+
+const styles = StyleSheet.create({});
