5c5d0276ba4aba436bf5eecd17529ededb99c1ee
andreyishchenko <andreyishchenko@sprut.ru>
2024-02-07 16:01:28 +0700

    add onboarding swipe

--- src/app/(days)/day2/index.tsx
+++ src/app/(days)/day2/index.tsx
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Link, Stack } from 'expo-router';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Day2Page = () => {
   return (
     <View>
+      <Stack.Screen options={{ title: 'Day 2: Onboarding' }} />
       <Text>Day2Page</Text>
+
+      <Link href="/day2/onboarding" asChild>
+        <Button title="Go to onboarding" />
+      </Link>
     </View>
   );
 };
--- src/components/core/DayListItem.tsx
+++ src/components/core/DayListItem.tsx
@@ -1,6 +1,5 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
 import { Link } from 'expo-router';
+import { Pressable, StyleSheet, Text } from 'react-native';
 
 interface DayListItemProps {
   day: any;
