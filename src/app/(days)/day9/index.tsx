323e92bc8f8e426666bfb44a6fe91ee953104340
andreyishchenko <andreyishchenko@sprut.ru>
2024-02-07 17:35:41 +0700

    auth start

---
+++ src/app/(days)/day9/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Day9Screen = () => {
+  return (
+    <View>
+      <Text>Day9Screen</Text>
+    </View>
+  );
+};
+
+export default Day9Screen;
+
+const styles = StyleSheet.create({});
